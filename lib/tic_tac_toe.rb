f2e7d325716544a45d4678098972cbeb7058b203
Joe Tustin <j.tustin@gmail.com>
2019-03-10 21:57:20 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -92,9 +92,9 @@ class TicTacToe
         position_3 = @board[win_index_3]
 
         if position_1 == "X" && position_2 == "X" && position_3 == "X"
-          return true
+          return combo
        elsif position_1 == "O"  && position_2 == "O" && position_3 == "O"
-          return true
+          return combo
         end
     end
     return false
